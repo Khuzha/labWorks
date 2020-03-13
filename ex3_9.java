07401a01b6d61fcdff3a5bacbb99c91ea2199de8
khuzha <mr.khuzha@gmail.com>
2020-03-13 23:26:33 +0300

    add printing of result in ex3_9

---
+++ ex3_9.java
@@ -0,0 +1,14 @@
+package com.khuzha.objects;
+
+public class ex3_9 {
+    public static void main(String[] args) {
+        int res = 1;
+
+        for (int digit = 1; digit < 10; digit++) {
+            if (digit % 3 == 0) {
+                res *= digit;
+            }
+        }
+        System.out.println("Произведение всех цифр, кратных 3, равно " + res);
+    }
+}
