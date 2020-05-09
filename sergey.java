cbda165d78d91dec01cba15dfe12e6fd02827b27
khuzha <mr.khuzha@gmail.com>
2020-05-10 00:12:56 +0300

    update code for sergay

--- sergey.java
+++ sergey.java
@@ -1,5 +1,4 @@
 class Sergey {
-
 	public static int findInd(int[] arr) {
 		int res = 0;
 
@@ -13,7 +12,8 @@ class Sergey {
 	public static void main(String[] args) {
 		int[] arr = new int[100];
 		for (int i = 0; i < arr.length; i++) {
-			arr[i] = (int) (Math.random() * (int) (Math.pow(2, 31) * Math.pow(-1, (int) (Math.random() * 2))));
+			arr[i] = (int) (Math.random()
+				* (int) (Math.pow(2, 31) * Math.pow(-1, (int) (Math.random() * 2))));
 			System.out.println(arr[i]);
 		}
 		System.out.println(findInd(arr));
